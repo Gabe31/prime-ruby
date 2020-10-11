d19cdc6f18c35125a72c836afb5b55e4621cf9d8
Gabe Aguilar <gabe.p.aguilar@gmail.com>
2020-10-11 13:17:13 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
-# Add  code here!
+def prime?(integer) 
+   if integer <= 1
+    return false
+   elsif integer <= 3
+    return true
+   else (2..integer/2).none? do |number| integer % number == 0
+    end 
+end
+end
